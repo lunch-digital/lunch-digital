b9bf7c554b9a09745e3b3c4b99f5b0dc6e798626
Richard Ekwonye <ekwonyenoob@gmail.com>
2022-05-05 23:02:53 +0100

    feat: mobile team memeber

--- src/components/TeamMemberCard/TeamMemberCard.js
+++ src/components/TeamMemberCard/TeamMemberCard.js
@@ -3,11 +3,13 @@ import styles from "./TeamMemberCard.module.scss"
 const TeamMemberCard = ({ name, role, avatar }) => (
 	<div className={styles.teamMemberCard}>
 		<img className={styles.avatar} src={avatar} alt={name} />
-		<div>
-			<span>{name}</span>
-		</div>
-		<div className={styles.role}>
-			<span>{role}</span>
+		<div className={styles.person}>
+			<div>
+				<span>{name}</span>
+			</div>
+			<div className={styles.role}>
+				<span>{role}</span>
+			</div>
 		</div>
 	</div>
 )
