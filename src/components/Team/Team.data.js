2dae2a882b844e1a809ae8fcfc4e7af616ae5ec0
Matthew Kendall Clark <101585948+Lunchmatt@users.noreply.github.com>
2022-05-28 16:35:52 -0400

    Alan Photo Update

--- src/components/Team/Team.data.js
+++ src/components/Team/Team.data.js
@@ -7,7 +7,7 @@ import person6 from "../../assets/images/vitor.jpeg"
 import person7 from "../../assets/images/angie.jpeg"
 import person8 from "../../assets/images/matheus.jpeg"
 import person9 from "../../assets/images/natalia2.png"
-import person10 from "../../assets/images/alan.jpeg"
+import person10 from "../../assets/images/alan2.jpg"
 
 export const TEAM_MEMBERS = [
 	{
