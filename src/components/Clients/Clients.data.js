5c844f410895dbe05261983f787a5750f9bb7249
Matthew Kendall Clark <101585948+Lunchmatt@users.noreply.github.com>
2022-11-05 11:31:30 -0400

    Updated Clients

--- src/components/Clients/Clients.data.js
+++ src/components/Clients/Clients.data.js
@@ -1,5 +1,6 @@
 export const CLIENTS = [
 	{ name: "Adobe", url: "https://adobe.com/" },
+	{ name: "Affirm", url: "https://affirm.com/" },
 	{ name: "Arena AI", url: "https://arena-ai.com/" },
 	{ name: "Assembled", url: "https://assembled.com/" },
 	{ name: "Better", url: "https://better.com/" },
@@ -7,6 +8,7 @@ export const CLIENTS = [
 	{ name: "Candy", url: "https://candy.com/" },
 	{ name: "Coast", url: "https://coastpay.com/" },
 	{ name: "Feather", url: "https://livefeather.com/" },
+	{ name: "Forme", url: "https://formefinancial.com/" },
 	{ name: "FourSquare", url: "https://foursquare.com/" },
 	{ name: "Frame.io", url: "https://frame.io/" },
 	{ name: "Grammarly", url: "https://grammarly.com/" },
@@ -15,6 +17,13 @@ export const CLIENTS = [
 	{ name: "Mantra Health", url: "https://mantrahealth.com/" },
 	{ name: "Moda Operandi", url: "https://modaoperandi.com/" },
 	{ name: "Rap Genius", url: "https://genius.com/" },
+	{ name: "Roofstock", url: "https://roofstock.com/" },
 	{ name: "ROKT", url: "https://www.rokt.com/" },
+	{ name: "Tenet", url: "https://www.tenet.com/" },
+	{ name: "TrialSpark", url: "https://www.trialspark.com/" }
+	{ name: "Updater", url: "https://www.updater.com/" },
+	{ name: "Underdog", url: "https://www.underdogfantasy.com/" },
+	{ name: "Vivi", url: "https://www.vivi.io/" },
 	{ name: "Warby Parker", url: "https://warbyparker.com/" },
+	{ name: "Whatnot", url: "https://whatnot.com/" },
 ]
