a9d9d6a85a7e3406c58dc2702cffb2f8c9f1b140
Matthew Kendall Clark <101585948+Lunchmatt@users.noreply.github.com>
2022-11-10 14:50:05 -0500

    Removing Shimon

--- src/components/Team/Team.data.js
+++ src/components/Team/Team.data.js
@@ -1,14 +1,13 @@
 import matthew from "../../assets/images/Matt2.jpeg"
 import person2 from "../../assets/images/Will4.jpg"
 import person3 from "../../assets/images/Lucy5.jpg"
-import person4 from "../../assets/images/shimon.jpeg"
-import person5 from "../../assets/images/jack5.jpg"
-import person6 from "../../assets/images/jason2.jpeg"
-import person7 from "../../assets/images/vitor.jpeg"
-import person8 from "../../assets/images/angie2.png"
-import person9 from "../../assets/images/matheus.jpeg"
-import person10 from "../../assets/images/alan2.jpg"
-import person11 from "../../assets/images/geo4.jpg"
+import person4 from "../../assets/images/jack5.jpg"
+import person5 from "../../assets/images/jason2.jpeg"
+import person6 from "../../assets/images/vitor.jpeg"
+import person7 from "../../assets/images/angie2.png"
+import person8 from "../../assets/images/matheus.jpeg"
+import person9 from "../../assets/images/alan2.jpg"
+import person10 from "../../assets/images/geo4.jpg"
 
 
 export const TEAM_MEMBERS = [
@@ -28,43 +27,38 @@ export const TEAM_MEMBERS = [
 		avatar: person3,
 	},
 	{
-		name: "Shimon Herman",
+		name: "Jack Totty",
 		role: "Associate",
 		avatar: person4,
 	},
 	{
-		name: "Jack Totty",
+		name: "Jason Lee-Menard",
 		role: "Associate",
 		avatar: person5,
 	},
 	{
-		name: "Jason Lee-Menard",
-		role: "Associate",
-		avatar: person6,
-	},
-	{
 		name: "Vitor Cestare",
 		role: "Senior Researcher",
-		avatar: person7,
+		avatar: person6,
 	},
 	{
 		name: "Angie Bortule",
 		role: "Researcher",
-		avatar: person8,
+		avatar: person7,
 	},
 	{
 		name: "Matheus Quibao",
 		role: "Researcher",
-		avatar: person9,
+		avatar: person8,
 	},
 	{
 		name: "Alan Garcia",
 		role: "Researcher",
-		avatar: person10,
+		avatar: person9,
 	},
 	{
 		name: "Geovanna de Aragão",
 		role: "Researcher",
-		avatar: person11,
+		avatar: person10,
 	},
 ]
